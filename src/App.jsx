bfa2f5c24fcdf6c9f6f8ba0c7e219a4d9be04f1e
mohammad <taskforcemmd@gmail.com>
2026-02-23 12:27:19 +0330

    inital project eduvia

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+    return (
+        <>
+            <h2 className={'text-3xl font-semibold text-blue-500'}>Hello world !!!</h2>
+        </>
+    )
+}
